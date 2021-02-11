9e4d60a32906633ee4a529bf5342c1595d3d8eee
eduardo <eduardo.augustolp@hotmail.com>
2021-02-11 19:24:11 -0300

    Alteração do endpoint de agências para o teste

--- src/main/java/com/bank/agencies/endpoint/AgenciesController.java
+++ src/main/java/com/bank/agencies/endpoint/AgenciesController.java
@@ -1,15 +1,20 @@
     package com.bank.agencies.endpoint;
 
-    import com.bank.agencies.domain.AgencyGatewayResponse;
-    import com.bank.agencies.domain.AgencyResponse;
-    import com.bank.agencies.usecase.FindAllAgenciesUseCase;
-    import org.springframework.http.HttpStatus;
-    import org.springframework.http.MediaType;
-    import org.springframework.http.ResponseEntity;
-    import org.springframework.web.bind.annotation.*;
-
     import java.util.List;
-    import java.util.stream.Collectors;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.bank.agencies.domain.AgencyGatewayResponse;
+import com.bank.agencies.domain.AgencyResponse;
+import com.bank.agencies.usecase.FindAllAgenciesUseCase;
 
     @RestController
     @RequestMapping(value = "/agencies", produces = MediaType.APPLICATION_JSON_VALUE)
@@ -23,18 +28,23 @@
 
         @GetMapping
         @ResponseStatus(HttpStatus.OK)
-        public ResponseEntity<List<AgencyResponse>> findAllAgencies() {
+        public ResponseEntity<Map<String, List<AgencyResponse>>> findAllAgencies() {
 
             List<AgencyGatewayResponse> agencies = findAllAgenciesUseCase.execute();
 
             List<AgencyResponse> agencyResponse = agencies.stream()
                     .map(agencyGateway -> AgencyResponse.AgencyResponseBuilder.anAgencyResponse()
                     .bank(agencyGateway.getBank())
-                    .city(agencyGateway.getCity())
                     .name(agencyGateway.getName())
-                    .state(agencyGateway.getState()).build())
+                    .city(agencyGateway.getCity()).build())
                     .collect(Collectors.toList());
+            
+            Map<String, List<AgencyResponse>> agenciesGroupByState =  agencyResponse.stream().collect(Collectors.groupingBy(AgencyResponse::getState));
+            
 
-            return new ResponseEntity<>(agencyResponse, HttpStatus.OK);
+            return new ResponseEntity<>(agenciesGroupByState, HttpStatus.OK);
         }
+        
+                
+        
     }
